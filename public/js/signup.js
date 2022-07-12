161e73ad5b2e6b60d808ec60dd6a62156f3f4899
Brian Alegre <brialegre@yahoo.com>
2022-07-11 20:58:22 -0700

    Added signup hbs and signup logic

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,23 +3,19 @@ const signupForm = document.querySelector('#signup-form');
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const first_name = document.querySelector('#firstname-signup').value.trim();
-  const last_name = document.querySelector('#lastname-signup').value.trim();
   const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const user_email = document.querySelector('#email-signup').value.trim();
+  const user_password = document.querySelector('#password-signup').value.trim();
 
   // Check all form variables
-  if (first_name && last_name && username && email && password) {
+  if (username && user_email && user_password) {
     try {
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({
           username,
-          first_name,
-          last_name,
-          email,
-          password,
+          user_email,
+          user_password,
         }),
         headers: { 'Content-Type': 'application/json' },
       });
