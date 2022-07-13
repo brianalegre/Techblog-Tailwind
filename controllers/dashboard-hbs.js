5bd4c47d099065e7728480c5663fe72545768ad5
Brian Alegre <brialegre@yahoo.com>
2022-07-13 10:17:31 -0700

    Added Dashboard hbs - still IP, working on dashboard routes - still IP

---
+++ controllers/dashboard-hbs.js
@@ -0,0 +1,43 @@
+// Import
+const router = require('express').Router();
+const { User, Blog } = require('../models');
+// const loggedIn = require('../utils/auth');
+
+//end point of /dashboard
+router.get('/dashboard', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      include: [{ all: true, nested: true }],
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!userData) {
+      return res.status(404).json({
+        message: 'This user ID does not exist. Please enter a valid user ID!',
+      });
+    }
+
+    const blogData = await Blog.findAll({
+      include: [{ model: User }],
+      where: {
+        user_id: req.params.id
+      }
+    })
+    // Render data
+    const users = userData.get({ plain: true });
+    const blogs = blogData.map(blog =>
+      blog.get({ plain: true })
+    )
+    res.render('myProfile', {
+      users,
+      blogs,
+      // logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
