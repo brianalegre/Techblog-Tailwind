f26cdf4b0ce53beb97be90a05807b2291c716674
Brian Alegre <brialegre@yahoo.com>
2022-07-15 22:22:59 -0700

    Add CSS to Comment Modal

--- public/js/addComment.js
+++ public/js/addComment.js
@@ -2,12 +2,40 @@
 const commentSection = document.querySelector('.comment-section');
 const commentBtn = document.querySelector('#comment-btn')
 
+// Target
+const commentModal = document.getElementById("commentModal");
+const addCommentBtn = document.getElementById("addComment-btn");
+const commentSpan = document.getElementsByClassName("commentClose")[0];
+const cancelBtns = document.getElementById("cancel-btn2")
+
+// When the user clicks on the button, open the modal
+addCommentBtn.onclick = function () {
+  commentModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+commentSpan.onclick = function () {
+  commentModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    commentModal.style.display = "none";
+  }
+}
+
+// When the user clicks on cancel, close the modal
+cancelBtns.onclick = function () {
+  commentModal.style.display = "none";
+}
+
 
 // Send Comment to Blog Post
 const commentHandler = async (id) => {
   // event.preventDefault();
 
-  const comment_content = document.querySelector('#comment').value.trim();
+  const comment_content = document.querySelector('#commentContent').value.trim();
   // const comment_post_date = '2022-03-30'
   const blog_id = id;
 
